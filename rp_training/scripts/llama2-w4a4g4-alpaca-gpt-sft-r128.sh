27e4ea46a72de7bdda59aeb626ee540b21ec0bb0
superdocker <hwanii@hanyang.ac.kr>
2025-12-15 11:57:44 +0900

    fix scripts

--- rp_training/scripts/llama2-w4a4g4-alpaca-gpt-sft-r128.sh
+++ rp_training/scripts/llama2-w4a4g4-alpaca-gpt-sft-r128.sh
@@ -3,7 +3,7 @@
 accelerate launch --config_file configs/zero3.yaml train.py \
   --gradient_accumulation_steps 1 \
   --per_device_train_batch_size 1 \
-  --model_name_or_path /raid/LLM/llama2-7b \
+  --model_name_or_path meta-llama/Llama-2-7b-hf \
   --w_format fp4_e2m1 \
   --a_format fp4_e2m1 \
   --g_format fp4_e2m1 \
